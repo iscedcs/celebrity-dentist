e535f2d16799184e5a3664ede0ec19ca4bc2bde7
Divine <105015390+LeoDivine@users.noreply.github.com>
2025-09-22 15:55:42 +0100

    feat:nexttoploader

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,5 +1,6 @@
 import { getCurrentUser } from "@/actions/auth";
 import { DashboardHeader } from "@/components/dashboard/dashboard-header";
+import { redirect } from "next/navigation";
 import React from "react";
 
 export default async function PrivateLayout({
@@ -9,9 +10,13 @@ export default async function PrivateLayout({
 }) {
   const user = await getCurrentUser();
 
+  if (!user) {
+    redirect("/sign-in");
+  }
+
   return (
     <div>
-      {user ? <DashboardHeader user={user} /> : <div>Loading...</div>}
+      {<DashboardHeader user={user} />}
       {children}
     </div>
   );
